d8304540bd6e9904d9f189d3a9238bd9361e2027
talentkingpython <lucky.godness.tan@gmail.com>
2025-06-24 19:47:45 +0900

    update feedback - Jun 24

--- components/dashboard/vendor-dashboard/booking/calendar/index.jsx
+++ components/dashboard/vendor-dashboard/booking/calendar/index.jsx
@@ -1,19 +1,13 @@
 "use client";
 
-import svgIcon from "@/components/data/svgIcon";
 import VendorDashboardLayout from "../../common/layout";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
-import { Eventcalendar, setOptions } from "@mobiscroll/react";
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import "@mobiscroll/react/dist/css/mobiscroll.min.css";
 import Filter from "../../common/Filter";
-
-setOptions({
-  theme: "ios",
-  themeVariant: "light",
-});
+import CustomEventCalendar from "../../common/CustomEventCalendar";
 
 const index = () => {
   function random(min, max) {
@@ -168,186 +162,6 @@ const index = () => {
         </div>
       </div>
     </VendorDashboardLayout>
-  );
-};
-
-const CustomEventCalendar = () => {
-  const [viewType, setViewType] = useState("month");
-
-  const myView = useMemo(
-    () => ({
-      timeline: {
-        type: viewType,
-        resolutionHorizontal:
-          viewType == "month" ? "week" : viewType == "week" ? "day" : "hour",
-      },
-    }),
-    [viewType]
-  );
-
-  const myEvents = useMemo(
-    () => [
-      {
-        start: "2025-06-02T00:00",
-        end: "2025-06-05T00:00",
-        title: "Event 1",
-        resource: 1,
-      },
-      {
-        start: "2025-06-06T00:00",
-        end: "2025-06-09T00:00",
-        title: "Event 2",
-        resource: 1,
-      },
-      {
-        start: "2025-06-10T00:00",
-        end: "2025-06-11T00:00",
-        title: "Event 2",
-        resource: 1,
-      },
-      {
-        start: "2025-06-12T00:00",
-        end: "2025-06-14T00:00",
-        title: "Event 2",
-        resource: 1,
-      },
-      {
-        start: "2025-06-10T09:00",
-        end: "2025-06-15T15:00",
-        title: "Event 2",
-        resource: 3,
-      },
-      {
-        start: "2025-06-12T00:00",
-        end: "2025-06-14T00:00",
-        title: "Event 3",
-        resource: 4,
-      },
-      {
-        start: "2025-06-15T07:00",
-        end: "2025-06-20T12:00",
-        title: "Event 4",
-        resource: 5,
-      },
-      {
-        start: "2025-06-03T00:00",
-        end: "2025-06-10T00:00",
-        title: "Event 5",
-        resource: 6,
-      },
-      {
-        start: "2025-06-10T08:00",
-        end: "2025-06-11T20:00",
-        title: "Event 6",
-        resource: 7,
-      },
-      {
-        start: "2025-06-22T00:00",
-        end: "2025-06-28T00:00",
-        title: "Event 7",
-        resource: 7,
-      },
-      {
-        start: "2025-06-08T00:00",
-        end: "2025-06-13T00:00",
-        title: "Event 8",
-        resource: 2,
-      },
-      {
-        start: "2025-06-25T00:00",
-        end: "2025-06-27T00:00",
-        title: "Event 9",
-        resource: 10,
-      },
-      {
-        start: "2025-06-20T00:00",
-        end: "2025-06-23T00:00",
-        title: "Event 10",
-        resource: 9,
-      },
-    ],
-    []
-  );
-
-  const myResources = useMemo(
-    () => [
-      {
-        id: 1,
-        name: "Deluxe Room",
-        color: "#e20000",
-      },
-      {
-        id: 2,
-        name: "Suite",
-        color: "#76e083",
-      },
-      {
-        id: 3,
-        name: "Standard Room",
-        color: "#4981d6",
-      },
-      {
-        id: 4,
-        name: "Cozy Beachfront Cottage",
-        color: "#e25dd2",
-      },
-      {
-        id: 5,
-        name: "Conference Hall",
-        color: "#1dab2f",
-      },
-      {
-        id: 6,
-        name: "Garden",
-        color: "#d6d145",
-      },
-      {
-        id: 7,
-        name: "Rooftop",
-        color: "#34c8e0",
-      },
-      {
-        id: 8,
-        name: "Historic Downtown Walking Tour",
-        color: "#9dde46",
-      },
-      {
-        id: 9,
-        name: "Yoga in the Park",
-        color: "#166f6f",
-      },
-      {
-        id: 10,
-        name: "Summer Music Festival",
-        color: "#f7961e",
-      },
-    ],
-    []
-  );
-
-  return (
-    <>
-      <div className="d-flex justify-end items-center mb-10 mr-10">
-        <select
-          className="form-select rounded-8 border-light justify-between text-16 px-15 w-140 text-14"
-          onChange={(event) => setViewType(event.target.value)}
-        >
-          <option value="month">Month</option>
-          <option value="week">Week</option>
-          <option value="day">Day</option>
-        </select>
-      </div>
-      <Eventcalendar
-        clickToCreate={true}
-        dragToCreate={true}
-        dragToMove={true}
-        dragToResize={true}
-        eventDelete={true}
-        view={myView}
-        data={myEvents}
-        resources={myResources}
-      />
-    </>
   );
 };
 
--- components/dashboard/vendor-dashboard/listings/non-property/AddEvent/Location.jsx
+++ components/dashboard/vendor-dashboard/listings/non-property/AddEvent/Location.jsx
@@ -1,8 +1,10 @@
 import MapFinder from "@/components/common/MapFinder";
+import { Checkbox } from "@mui/material";
 import { useState } from "react";
 
 const Location = () => {
-   const [location, setLocation] = useState();
+  const [location, setLocation] = useState();
+  const [isThirdParty, setIsThirdParty] = useState(false);
 
   const locationList = [
     {
@@ -27,23 +29,53 @@ const Location = () => {
 
   return (
     <div className="row y-gap-10 x-gap-10">
-      <h1 className="text-20 lh-14 fw-600">Venue/Location</h1>
-
-      <div className="col-sm-6 mt-5">
-        <h1 className="text-14 lh-12 fw-500">Location</h1>
-        <select className="form-select rounded-8 border-light px-15 py-10 justify-between text-14 w-full mt-10" onChange={(e) => setLocation(locationList[e.target.value])}>
-          <option value="" defaulValue>
-            Select location
-          </option>
-          <option value="0">
-            1523 Willow Creek Rd Asheville, NC 28806, USA
-          </option>
-          <option value="1">8712 Maple Hollow Dr Plano, TX 75024, USA</option>
-          <option value="2">
-            4307 Sunset Ridge Blvd San Diego, CA 92117, USA
-          </option>
-        </select>
+      <h1 className="text-20 lh-1 fw-600">Event Venue/Location</h1>
+      <div className="text-14 lh-1 text-light-1">
+        Select a location from the drop down below if you own the location or
+        Add a new Event Location if a 3rd Party Location
       </div>
+
+      <div className="col-12 d-flex items-center mt-10 gap-2">
+        <Checkbox
+          className="px-0 py-0"
+          value={isThirdParty}
+          onChange={(e) => {
+            setIsThirdParty(e.target.checked);
+            setLocation({ city: "", state: "", area: "", zip: "" });
+          }}
+        />
+        <div className="text-14 lh-12 fw-500">Third Party Location</div>
+      </div>
+
+      {isThirdParty ? (
+        <div className="col-sm-6 mt-5">
+          <h1 className="text-14 lh-12 fw-500">Enter Location Address</h1>
+          <input
+            className="border-light rounded-8 py-5 px-15 w-full mt-10"
+            type="text"
+            placeholder="Enter location address"
+          />
+        </div>
+      ) : (
+        <div className="col-sm-6 mt-5">
+          <h1 className="text-14 lh-12 fw-500">Location</h1>
+          <select
+            className="form-select rounded-8 border-light px-15 py-10 justify-between text-14 w-full mt-10"
+            onChange={(e) => setLocation(locationList[e.target.value])}
+          >
+            <option value="" defaulValue>
+              Select location
+            </option>
+            <option value="0">
+              1523 Willow Creek Rd Asheville, NC 28806, USA
+            </option>
+            <option value="1">8712 Maple Hollow Dr Plano, TX 75024, USA</option>
+            <option value="2">
+              4307 Sunset Ridge Blvd San Diego, CA 92117, USA
+            </option>
+          </select>
+        </div>
+      )}
 
       <div className="col-sm-3 mt-5">
         <h1 className="text-14 lh-12 fw-500">City</h1>
@@ -51,8 +83,9 @@ const Location = () => {
           className="border-light rounded-8 py-5 px-15 w-full mt-10"
           type="text"
           placeholder="Enter city"
+          onChange={(e) => setLocation({ ...location, city: e.target.value })}
           value={location?.city}
-          readOnly
+          readOnly={!isThirdParty}
         />
       </div>
 
@@ -62,8 +95,9 @@ const Location = () => {
           className="border-light rounded-8 py-5 px-15 w-full mt-10"
           type="text"
           placeholder="Enter state"
+          onChange={(e) => setLocation({ ...location, state: e.target.value })}
           value={location?.state}
-          readOnly
+          readOnly={!isThirdParty}
         />
       </div>
 
@@ -73,8 +107,9 @@ const Location = () => {
           className="border-light rounded-8 py-5 px-15 w-full mt-10"
           type="text"
           placeholder="Enter neighborhood or area"
+          onChange={(e) => setLocation({ ...location, area: e.target.value })}
           value={location?.area}
-          readOnly
+          readOnly={!isThirdParty}
         />
       </div>
 
@@ -84,8 +119,9 @@ const Location = () => {
           className="border-light rounded-8 py-5 px-15 w-full mt-10"
           type="text"
           placeholder="Enter postal code"
+          onChange={(e) => setLocation({ ...location, zip: e.target.value })}
           value={location?.zip}
-          readOnly
+          readOnly={!isThirdParty}
         />
       </div>
 
