f558c17a18486ea184164abdb81de74638f64ec2
brightkid007 <brightkid.successfuture93@gmail.com>
2025-07-06 22:58:56 -0700

    add metadata cms/add

--- app/agent/cms/add/page.jsx
+++ app/agent/cms/add/page.jsx
@@ -1,14 +1,12 @@
-"use client";
-
 import dynamic from "next/dynamic";
 import AddStaticPage from "@/components/agent/cms/manage/AddStaticPage";
 import AddBanner from "@/components/agent/cms/manage/AddBanner";
 import { useSearchParams } from "next/navigation";
 
-// export const metadata = {
-//   title: "Agent Dashboard || Plist Travel",
-//   description: "Agent Dashboard for Plist Travel",
-// };
+export const metadata = {
+  title: "Agent Dashboard || Plist Travel",
+  description: "Agent Dashboard for Plist Travel",
+};
 
 const index = () => {
   const service = useSearchParams().get("service");
