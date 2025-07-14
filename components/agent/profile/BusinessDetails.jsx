a7568d12c3603183aa9d2c03cee4d1b71749545f
talentkingpython <lucky.godness.tan@gmail.com>
2025-07-14 10:31:56 +0900

    update for location addressess

--- components/agent/profile/BusinessDetails.jsx
+++ components/agent/profile/BusinessDetails.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 const Financial = () => {
+  const [locationCount, setLocationCount] = useState(1);
   return (
     <div className="row y-gap-10 x-gap-10 bg-white px-20 py-20 rounded-8">
       <div className="text-20 fw-600 lh-1">Business Information</div>
@@ -24,62 +27,150 @@ const Financial = () => {
         />
       </div>
 
-      <div className="col-12 text-14 lh-14 fw-500 mt-10">Business Address</div>
-
-      <div className="col-md-6 col-sm-12">
-        <h1 className="text-13 lh-14 fw-500">Address Line 1</h1>
+      <div className="text-18 fw-500 mt-10">Headquarters Business Address</div>
+      <div className="col-12">
+        <h1 className="text-13 lh-14 fw-500">Address</h1>
         <input
           className="border-light rounded-8 py-5 px-15 w-full mt-5"
           type="text"
-          placeholder="Enter address line 1"
+          placeholder="Enter Address"
         />
       </div>
-
       <div className="col-md-6 col-sm-12">
-        <h1 className="text-13 lh-14 fw-500">Address Line 2</h1>
-        <input
-          className="border-light rounded-8 py-5 px-15 w-full mt-5"
-          type="text"
-          placeholder="Enter address line 2"
-        />
-      </div>
-
-      <div className="col-md-3 col-sm-6">
         <h1 className="text-13 lh-14 fw-500">City</h1>
         <input
-          className="border-light rounded-8 h-45 px-15 w-full mt-5"
+          className="border-light rounded-8 py-5 px-15 w-full mt-5"
           type="text"
-          placeholder="Enter city"
+          placeholder="Enter City"
         />
       </div>
-
-      <div className="col-md-3 col-sm-6">
-        <h1 className="text-13 lh-14 fw-500">State / Province</h1>
+      <div className="col-md-6 col-sm-12">
+        <h1 className="text-13 lh-14 fw-500">State/Province</h1>
         <input
-          className="border-light rounded-8 h-45 px-15 w-full mt-5"
+          className="border-light rounded-8 py-5 px-15 w-full mt-5"
           type="text"
-          placeholder="Enter state / province"
+          placeholder="Enter State/Province"
         />
       </div>
-
-      <div className="col-md-3 col-sm-6">
-        <h1 className="text-13 lh-14 fw-500">Postal Code</h1>
+      <div className="col-md-6 col-sm-12">
+        <h1 className="text-13 lh-14 fw-500">Zip Code / Postal Code</h1>
         <input
-          className="border-light rounded-8 h-45 px-15 w-full mt-5"
+          className="border-light rounded-8 py-5 px-15 w-full mt-5"
           type="text"
-          placeholder="Enter postal code"
+          placeholder="Enter Zip Code / Postal Code"
         />
       </div>
-
-      <div className="col-md-3 col-sm-6">
+      <div className="col-md-3 col-sm-12">
         <h1 className="text-13 lh-14 fw-500">Country</h1>
-        <select className="form-select border-light rounded-8 h-45 px-15 w-full mt-5">
-          <option value="us">United State</option>
-          <option value="ca">Canada</option>
+        <select className="form-select rounded-8 border-light justify-between py-10 mt-5 px-20 w-full text-14">
+          <option value="USA">USA</option>
+          <option value="Canada">Canada</option>
+          <option value="Mexico">Mexico</option>
         </select>
       </div>
+      <div className="col-md-3 col-sm-12">
+        <h1 className="text-13 lh-14 fw-500">Region</h1>
+        <select className="form-select rounded-8 border-light justify-between py-10 mt-5 px-20 w-full text-14">
+          <option value="northeast">Northeast</option>
+          <option value="southeast">Southeast</option>
+          <option value="midwest">Midwest</option>
+          <option value="southwest">Southwest</option>
+          <option value="west">West</option>
+        </select>
+      </div>
+      <div className="col-12 mt-15">
+        <div className="row justify-between items-center">
+          <div className="text-18 fw-500 mt-10 col-auto">
+            Location Addresses
+          </div>
+          <div className="d-flex col-sm-auto pr-50">
+            <button
+              className="button border-light rounded-4 text-13 fw-500 px-10 py-5"
+              onClick={() => setLocationCount(locationCount + 1)}
+            >
+              <span className="material-symbols-outlined mr-10 text-15 fw-500">
+                add_circle
+              </span>
+              Add Address
+            </button>
+          </div>
+        </div>
+        {Array.from({ length: locationCount }).map((_, index) => (
+          <div className="row border-bottom-light pb-20" key={index}>
+            {/* <div
+              className="row justify-between items-center mt-10"
+              key={index}
+            >
+              <input
+                className="border-light rounded-8 py-5 px-15 w-full"
+                type="text"
+                placeholder="Enter Address"
+              />
+              <button
+                disabled={locationCount === 1}
+                className="ml-10 pt-5 px-5"
+                onClick={() => setLocationCount(locationCount - 1)}
+              >
+                <span className="material-symbols-outlined text-red-2 text-15 fw-500">
+                  delete
+                </span>
+              </button>
+            </div> */}
+            <div className="col-12 mt-15">
+              <h1 className="text-13 lh-14 fw-500">Address</h1>
+              <input
+                className="border-light rounded-8 py-5 px-15 w-full mt-5"
+                type="text"
+                placeholder="Enter Address"
+              />
+            </div>
+            <div className="col-md-6 col-sm-12 mt-15">
+              <h1 className="text-13 lh-14 fw-500">City</h1>
+              <input
+                className="border-light rounded-8 py-5 px-15 w-full mt-5"
+                type="text"
+                placeholder="Enter City"
+              />
+            </div>
+            <div className="col-md-6 col-sm-12 mt-15">
+              <h1 className="text-13 lh-14 fw-500">State/Province</h1>
+              <input
+                className="border-light rounded-8 py-5 px-15 w-full mt-5"
+                type="text"
+                placeholder="Enter State/Province"
+              />
+            </div>
+            <div className="col-md-6 col-sm-12 mt-15">
+              <h1 className="text-13 lh-14 fw-500">Zip Code / Postal Code</h1>
+              <input
+                className="border-light rounded-8 py-5 px-15 w-full mt-5"
+                type="text"
+                placeholder="Enter Zip Code / Postal Code"
+              />
+            </div>
+            <div className="col-md-3 col-sm-12 mt-15">
+              <h1 className="text-13 lh-14 fw-500">Country</h1>
+              <select className="form-select rounded-8 border-light justify-between py-10 mt-5 px-20 w-full text-14">
+                <option value="USA">USA</option>
+                <option value="Canada">Canada</option>
+                <option value="Mexico">Mexico</option>
+              </select>
+            </div>
+            <div className="col-md-3 col-sm-12 mt-15">
+              <h1 className="text-13 lh-14 fw-500">Region</h1>
+              <select className="form-select rounded-8 border-light justify-between py-10 mt-5 px-20 w-full text-14">
+                <option value="northeast">Northeast</option>
+                <option value="southeast">Southeast</option>
+                <option value="midwest">Midwest</option>
+                <option value="southwest">Southwest</option>
+                <option value="west">West</option>
+              </select>
+            </div>
+          </div>
+        ))}
+      </div>
 
-      <div className="col-12">
+      <div className="col-12 mt-10">
         <h1 className="text-13 lh-14 fw-500">Business Description</h1>
         <textarea
           className="border-light rounded-8 py-5 px-15 w-full mt-5"
